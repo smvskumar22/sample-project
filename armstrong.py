72b7ba7f1f4e2f37d815698af4ad6f831c19a6ee
smvskumar22 <smvskumar22@gmail.com>
2026-03-14 11:16:05 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,14 +1,14 @@
-n=int(input("enter a number:"))
-temp=n
+n1=int(input("enter a number:"))
+temp=n1
 ams=0
-st=str(n)
+st=str(n1)
 digits=0
 for i in st:
     digits=digits+1
 while n>0:
     rem=n%10
     ams=ams+(rem**digits)
-    n=n//10
+    n1=n1//10
 if  temp==ams:
     print("armstrong")
 else:
